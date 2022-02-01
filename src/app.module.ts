0b1d12e19f85605504574d8eaa2f4c43101c8a3a
Mhayk Whandson <hi@mhayk.com>
2022-02-01 16:16:23 +0000

    Small project with Nextjs + Bull + Cache + Sequelize + Sqlite3
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { BullModule } from '@nestjs/bull';
+import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { join } from 'path/posix';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TweetsModule } from './tweets/tweets.module';
+import { MailingModule } from './mailing/mailing.module';
+
+@Module({
+  imports: [
+    ScheduleModule.forRoot(),
+    BullModule.forRoot({
+      redis: {
+        host: 'redis',
+        port: 6379,
+      }
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: join(__dirname, 'database.sqlite'),
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    TweetsModule,
+    MailingModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
---
+++ src/mailing/mailing.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { SendMailWithTweetsJob } from './send-mail-with-tweets.job'
+
+@Module({
+    providers: [SendMailWithTweetsJob]
+})
+export class MailingModule { }
+
+
+// task that verify new tweets every 5 minutes
